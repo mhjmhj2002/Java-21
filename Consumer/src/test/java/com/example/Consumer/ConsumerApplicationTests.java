3e869637ad91658d1e0104a3a2f05d5dbb990b8d
mhj <mhjmhj2002@gmail.com>
2025-10-25 11:39:49 -0300

    Add ajuste config teste

--- Consumer/src/test/java/com/example/Consumer/ConsumerApplicationTests.java
+++ Consumer/src/test/java/com/example/Consumer/ConsumerApplicationTests.java
@@ -2,8 +2,12 @@ package com.example.Consumer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.example.consumer.ConsumerApplication;
 
 @SpringBootTest
+@ContextConfiguration(classes = ConsumerApplication.class)
 class ConsumerApplicationTests {
 
 	@Test
